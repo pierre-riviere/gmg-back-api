b59b126aae5636d59b115ce85e1bcca9a4379ab1
Pierre <pierreriviere@hotmail.com>
2020-11-13 17:24:54 +0100

    ✨ Add User and Task resource api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\TaskController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::resources([
+    "users" => UserController::class,
+    "tasks" => TaskController::class,
+]);
