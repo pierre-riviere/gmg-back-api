619c87cf05d2531d813b28ce0d703fd7d5d50732
Pierre <pierreriviere@hotmail.com>
2020-11-14 13:30:16 +0100

    ⚡️Implement crud methods for Task

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Task;
+use Exception;
+use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
@@ -13,17 +16,7 @@ class TaskController extends Controller
      */
     public function index()
     {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
+        return Task::all();
     }
 
     /**
@@ -34,51 +27,114 @@ class TaskController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        try {
+            $rules = [
+                "name" => ["required", "max:255"],
+                "description" => ["required", "max:255"],
+                "status" => ["required", "max:255"],
+                "user_id" => ["required", "exists:users,id"],
+            ];
+
+            // validate request input
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "code" => "invalid_data",
+                        "error" => $validator->errors()->all(),
+                    ],
+                    400
+                );
+            }
+
+            // validate request input
+            $validator = Validator::make($request->all(), $rules);
+
+            return response()->json(
+                [
+                    "code" => "created_task",
+                    "task" => Task::create($request->all()),
+                ],
+                201
+            );
+        } catch (Exception $err) {
+            return response()->json([
+                "code" => "not_created_task",
+                "error" => $err->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Task $task)
     {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
+        return $task;
     }
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, Task $task)
     {
-        //
+        try {
+            // rules for user input validation
+            $rules = [
+                "name" => ["filled", "max:255"],
+                "description" => ["filled", "max:255"],
+                "status" => ["filled", "max:255"],
+                "user_id" => ["filled", "exists:users,id"],
+            ];
+
+            // validate request input
+            $validator = Validator::make($request->all(), $rules);
+
+            if ($validator->fails()) {
+                return response()->json(
+                    [
+                        "code" => "invalid_data",
+                        "error" => $validator->errors()->all(),
+                    ],
+                    400
+                );
+            }
+
+            // update task
+            $task->update($request->all());
+
+            return [
+                "code" => "updated_task",
+                "task" => $task,
+            ];
+        } catch (Exception $err) {
+            return response()->json([
+                "code" => "not_updated_task",
+                "error" => $err->getMessage(),
+            ]);
+        }
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param  Task  $task
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Task $task)
     {
-        //
+        $task->delete();
+        return [
+            "code" => "deleted_task",
+            "task" => $task,
+        ];
     }
 }
